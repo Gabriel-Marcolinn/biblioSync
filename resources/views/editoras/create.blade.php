90245835c5b59bbb63b8e0d6ee76890620e9a5ef
Gabriel Marcolin <gabrielgmarcolin12@gmail.com>
2024-09-26 21:59:28 -0300

    Ajustado editoras.create

--- resources/views/editoras/create.blade.php
+++ resources/views/editoras/create.blade.php
@@ -1,27 +1,30 @@
 <x-app-layout>
-    <html>
-        <head>
-
-        </head>
-        <body>
-            <form action="">
-                <div class="container">
-                    <div class="col-6 mx-auto">
-                        <div class="mb-3">
-                            <label for="nome" class="form-label labels"><strong>Nome:</strong></label>
-                            <input type="text" class="form-control" id="nome" placeholder="Digite o nome da editora...">
-                        </div>
-                        <div class="mb-3">
-                            <label for="cnpj" class="form-label"><strong>CNPJ:</strong></label>
-                            <input type="text" class="form-control" id="cnpj" placeholder="Digite o CNPJ da editora...">
-                        </div>
-                        <div class="text-center">
-                            <button type="submit" class="btn btn-success">Enviar</button>
-                            <button type="reset" class="btn btn-danger">Apagar</button>
-                        </div>
-                    </div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastrar Editora</title>
+</head>
+<body>
+    <form action="{{route('editoras.store')}}" method="POST">
+        <div class="container">
+            <div class="col-6 mx-auto">
+                <div class="mb-3">
+                    <label for="nome" class="form-label labels"><strong>Nome:</strong></label>
+                    <input type="text" class="form-control" id="nome" name="nome" placeholder="Digite o nome da editora...">
                 </div>
-            </form>
-        </body>
-    </html>
+                <div class="mb-3">
+                    <label for="cnpj" class="form-label"><strong>CNPJ:</strong></label>
+                    <input type="text" class="form-control" id="cnpj" name="cnpj" placeholder="Digite o CNPJ da editora...">
+                </div>
+                <div class="text-center">
+                    <button type="submit" class="btn btn-success">Enviar</button>
+                    <button type="reset" class="btn btn-danger">Apagar</button>
+                </div>
+            </div>
+        </div>
+    </form>
+</body>
+</html>
 </x-app-layout>
